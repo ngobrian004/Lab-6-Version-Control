798f6a86f2ba1e6628d702f58e4a7134e974f1b1
ngobrian004 <ngobrian@ufl.edu>
2023-03-06 14:09:14 -0500

    Here is the original code

---
+++ main.py
@@ -0,0 +1,34 @@
+# Brian Ngo
+
+def encode(password):
+    conversion_table = {"0": "3", '1': "4", '2': "5", '3': "6", '4': "7", '5': "8", '6': "9", '7': "0", '8': "1", '9': "2",}
+    new_password = ""
+    for i in password:
+        new_password += conversion_table[i]
+    return new_password
+
+def decode():
+    pass
+
+if __name__ == "__main__":
+    loop = True
+    while loop:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            original_password = input("Please enter your password to encode: ")
+            encoded_password = encode(original_password)
+            print("Your password has been encoded and stored!")
+            print()
+        if option == 2:
+            original_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+            print()
+        if option == 3:
+            exit()
